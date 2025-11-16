d3424aef2a9d71e7254704ddb1ed521151076386
Tomer Shoham <tshoham@fireblocks.com>
2025-11-16 15:04:42 +0200

    feat: add scavenger hunt note constant for user registration

--- src/constants.ts
+++ src/constants.ts
@@ -6,10 +6,11 @@ export const midnightClaimAddress = "https://mainnet.prod.gd.midnighttge.io"; //
 
 export const scavengerHuntBaseUrl = "https://scavenger.prod.gd.midnighttge.io";
 
+export const scavengerHuntNote = "Registering address for NIGHT scavenger hunt";
+
 export const termsAndConditionsHash =
   "31a6bab50a84b8439adcfb786bb2020f6807e6e8fda629b424110fc7bb1c6b8b";
 
 export const nightTokenName = "NIGHT";
 
 export const tokenTransactionFee = 200000;
-
